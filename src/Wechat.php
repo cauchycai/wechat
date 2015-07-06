1c03635ab7f2d9c3b4d9d6104f7786a2831cb31c
Cauchy Cai <cauchy.cai@gmail.com>
2015-07-06 18:22:49 +0800

    Composer require wechat-crypto

--- src/Wechat.php
+++ src/Wechat.php
@@ -13,6 +13,24 @@ namespace Wechat;
 
 class Wechat
 {
+    const MSG_TYPE_TEXT = 'text';
+    const MSG_TYPE_LINK = 'link';
+    const MSG_TYPE_NEWS = 'news';
+    const MSG_TYPE_EVENT = 'event';
+    const MSG_TYPE_IMAGE = 'image';
+    const MSG_TYPE_VOICE = 'voice';
+    const MSG_TYPE_VIDEO = 'video';
+    const MSG_TYPE_MUSIC = 'music';
+    const MSG_TYPE_LOCATION = 'location';
+    const EVENT_SUBSCRIBE = 'subscribe';
+    const EVENT_UNSUBSCRIBE = 'unsubscribe';
+    const EVENT_VIEW = 'view';
+    const EVENT_CLICK = 'click';
+    const EVENT_SCAN = 'scan';
+    const EVENT_LOCATION = 'location';
+    const EVENT_SCANCODE_PUSH = 'scancode_push';
+    const EVENT_SCANCODE_WAITMSG = 'scancode_waitmsg';
+
     public static function checkSignature(array $query, $token)
     {
         $signature = $query['signature'];
--- tests/WechatTest.php
+++ tests/WechatTest.php
@@ -1,13 +1,21 @@
 <?php
 
-namespace Wechat\Test;
+namespace \Wechat\Test;
 
 use \Wechat\Wechat;
+use \WechatCrypto\Crypto;
 
 class WechatTest extends \PHPUnit_Framework_TestCase
 {
+    private $token;
+    private $encodingAesKey;
+    private $appId;
+
     public function setUp()
     {
+        $this->token = 'pamtest';
+        $this->encodingAesKey = 'abcdefghijklmnopqrstuvwxyz0123456789ABCDEFG';
+        $this->appId = 'wxb11529c136998cb6';
     }
 
     /**
